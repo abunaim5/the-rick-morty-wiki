bdb5cd8a140536e15ee0a4770258d2322c5f8945
Abu Naim <abunaim140@gmail.com>
2025-05-23 23:12:04 +0600

    change the background banner direction banner to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import Banner from "@/components/Banner/Banner";
 
 export default function Home() {
   return (
-    <div className='min-h-screen object-contain bg-right-bottom bg-no-repeat bg-[#191D29]' style={{ backgroundImage: "url('/images/spiral.png')" }}>
+    <div className='min-h-screen relative object-contain bg-right-bottom bg-no-repeat bg-[#191D29]' style={{ backgroundImage: "url('/images/spiral.png')" }}>
+      <div className='h-screen w-full absolute z-10 bg-cover bg-top bg-no-repeat bg-[lightgray]/50' style={{ backgroundImage: "url('/images/banner.png')" }}>
+        <div className='absolute w-full h-screen bg-[#191D29]/85 bg-[radial-gradient(90.85%_49.94%_at_50%_50%,rgba(217,217,217,0)_0%,rgba(25,29,41,0.9)_100%)]'>
+          <div className='absolute z-20 w-full h-screen opacity-60 bg-[lightgray] bg-no-repeat mix-blend-saturation' style={{ backgroundImage: "url('/images/texture.png')" }} />
+        </div>
+      </div>
+      <div className='w-full absolute right-0 bottom-0 z-10 bg-contain  bg-no-repeat bg-[lightgray]/50' />
       <main className=''>
         <Banner />
         <div className='h-screen'></div>
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -5,11 +5,7 @@ const Banner = () => {
     const gradientCls = 'bg-gradient-to-r from-[#9DFE00] to-[#14D9E5]'
 
     return (
-        <section className='h-screen relative bg-cover bg-top bg-no-repeat bg-[lightgray]/50' style={{ backgroundImage: "url('/images/banner.png')" }}>
-            <div className='absolute w-full h-screen bg-[#191D29]/85 bg-[radial-gradient(90.85%_49.94%_at_50%_50%,rgba(217,217,217,0)_0%,rgba(25,29,41,0.9)_100%)]'>
-                <div className='absolute z-10 w-full h-screen opacity-60 bg-[lightgray] bg-no-repeat mix-blend-saturation' style={{ backgroundImage: "url('/images/texture.png')" }} />
-            </div>
-
+        <section className='h-screen relative'>
             <Image className='absolute z-30 left-1/2 transform -translate-x-1/2 top-14' width={227} height={48} alt='logo' src='/images/logo.png' />
 
             <div className='absolute w-full z-40 top-1/2 transform -translate-y-1/2 text-white'>
